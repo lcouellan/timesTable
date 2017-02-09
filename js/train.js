27380e22750f9945a560f2b5a4260384660fadb9
plabadille <plabadille@gmail.com>
2017-02-09 14:23:00 +0100

    add the statistic displaying for training

--- js/train.js
+++ js/train.js
@@ -7,10 +7,14 @@ var learning = {
     //-----------------------------------
     //Game methods call by front listener
     //-----------------------------------
+    //init function lauching the game
     play: function(tableNumber) {
+      //generate all the operation
       this.compute(tableNumber);
+      //set the first round (0+1) and so start it.
       store.round++;
     },
+    //check the user response and alter the game in consequence
     playRound: function(roundNumber, userChoice) {
       //we keep playing unless we have already done 10 turn
       if (roundNumber <= 10){
@@ -19,6 +23,7 @@ var learning = {
         if (userChoice != store.operations[roundNumber-1].result) {
           store.operations[roundNumber-1].error++;
         } else { //right answer
+          //we archive userChoices then reset it.
           store.operations[roundNumber-1].userChoices = store.userChoices;
           store.userChoices = [];
           store.round++;
@@ -30,6 +35,7 @@ var learning = {
     //-----------------------------------
     //Game methods call by backend
     //-----------------------------------
+    //generate 10 operations form a given table
     compute: function(tableNumber) {
       store.table = tableNumber;
 
@@ -42,8 +48,7 @@ var learning = {
         for (c=1; c<=10; c++) {
           choices[c-1] = tableNumber * c;
         }
-
-        // operations[m-1] = operation;
+        //we store the operation
         store.operations[m-1] = {
           multiplier : m,
           result : tableNumber * m,
@@ -53,16 +58,18 @@ var learning = {
           time : 0
         };
       }
-
+      //we mix the operation
       store.operations = this.mix(store.operations);
+    },
+    //when called this function store the result from the store to local storage
+    end: function() {
+      
 
-      },
-    // end: function() {
-
-    // },
+    },
     //-----------------------------------
     //Generic methods (tools)
     //-----------------------------------
+    //mix an array by doing size² permutation in it.
     mix: function(array) {
       for (i=0; i<=array.length*array.length; i++) {
         let random = Math.floor((Math.random() * (array.length)));
